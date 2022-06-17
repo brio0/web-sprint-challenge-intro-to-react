ccfb887345b9258d91085f00063e45b1b16a4dc0
Niki Balajadia <niki@Nikis-MacBook-Pro.local>
2022-06-16 22:38:36 -1000

    styles done

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,13 +2,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledDiv = styled.div`
+    color: ${props => props.theme.secondaryColor};
+    background-color: ${props => props.theme.primaryColor};
+    display: flex;
+    justify-content: space-between;
+    width: 50%;
+    margin: 1% 25% 1%;
+
+     @media only screen and ${props => props.theme.breakpointMobile} {
+        display: flex;
+        flex-direction: column;
+   }
+`
+
 const Character = (props) => {
     const { name, birth } = props
     return (
-        <div>
+        <StyledDiv>
             <h3>{name}</h3>
             <p>{birth}</p>
-        </div>
+        </StyledDiv>
     )
 }
 
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,3 +1,8 @@
 export default {
-    primaryColor: 'blue',
+    primaryColor: 'aqua',
+    secondaryColor: 'cadetblue',
+    tertiaryColor: 'orange',
+    black: 'black',
+    white: 'white',
+    breakpointMobile: "(max-width: 550px)"
 }
