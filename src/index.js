12576b3600ae1ab4c3c49fc63791a93a6f98c96c
Niki Balajadia <niki@Nikis-MacBook-Pro.local>
2022-06-16 16:58:16 -1000

    theme made

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "./App.css";
+import { ThemeProvider } from 'styled-components'
+import theme from './theme/theme'
 
 import { worker } from "./mocks/browser";
 worker.start();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<ThemeProvider theme={theme}><App /></ThemeProvider>, document.getElementById("root"));
